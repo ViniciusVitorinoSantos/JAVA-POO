dade3d33608f63d22d59efc0bd97dc252f1f757b
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-08 17:54:30 -0300

    JAVA POO - aula 04 métodos especiais get set construtor

---
+++ JavaPooAula04/src/javapooaula04/JavaPooAula04.java
@@ -0,0 +1,12 @@
+package javapooaula04;
+
+public class JavaPooAula04 {
+    
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.setModelo("Bic");
+        c1.setPonta(0.5f);
+        c1.status();
+    }
+    
+}
