bc47a39bc1089a1a851d8a89f68ab850f4b4c75d
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2025-03-10 00:43:33 -0300

    aula 05 JAVA POO exemplo pratico

--- JavaPooAula05/src/javapooaula05/ContaBanco.java
+++ JavaPooAula05/src/javapooaula05/ContaBanco.java
@@ -17,31 +17,83 @@ public class ContaBanco {
     private float saldo;
     private boolean status;
 
-   
-
     //METODOS
-    public void abrirConta() {
-
+    
+     public void estadoAtual() {
+         System.out.println("-----------------------------");
+         System.out.println("Conta: "  + this.getNumConta());
+         System.out.println("Tipo: " + this.getTipo());
+         System.out.println("Dono: " + this.getDono());
+         System.out.println("Saldo: " + this.getSaldo());
+         System.out.println("Status: " + this.getStatus());
+         System.out.println("-----------------------------");
+         
     }
-
+    
+    public void abrirConta(String t) {
+        this.setTipo(t);
+        this.setStatus(true);
+        if ("CC".equals(t)) {
+            //this.saldo = 50f; diretamente atuando no atributo;
+            this.setSaldo(50f); // utilizando o metodo set para alterar o atributo;
+        } else if ("CP".equals(t)) {
+            //this.saldo = 150f;
+            this.setSaldo(150f);
+            
+        }
+        System.out.println("Conta aberta com sucesso ! ");
+    }
+    
     public void fecharConta() {
-
+        if (this.getSaldo() > 0) {
+            System.out.println("Conta não pode ser fechada pois ainda tem saldo ! ");
+        } else if (this.getSaldo() < 0) {
+            System.out.println("Conta não pode ser fechada pois tem debitos ! ");
+        } else {
+            this.setStatus(!true);
+            System.out.println("Conta fechada com sucesso ! ");
+        }
     }
-
-    public void depositar() {
-
+    
+    public void depositar(float v) {
+        if (this.getStatus()) {
+            //this.saldo = this.saldo + v;
+            this.setSaldo(this.getSaldo() + v);
+            System.out.println("Depósito realizado com sucesso na conta de " + this.getDono());
+        } else {
+            System.out.println("Impossivle depositar em uma conta fechada ! ");
+        }
     }
-
-    public void sacar() {
-
+    
+    public void sacar(float v) {
+        if (this.getStatus()) {
+            if (this.getSaldo() >= v) {
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saque realizado na conta de " + this.getDono() + "no valor de " + v);
+            } else {
+                System.out.println("Saldo insuficiente ! ");
+            }
+        } else {
+            System.out.println("Impossivel sacar de uma conta fechada !");
+        }
     }
-
+    
     public void pagarMensal() {
-
+        int v = 0;
+        if (this.getTipo() == "CC") {
+            v = 12;
+        } else if(this.getTipo() == "CP") {
+            v = 20;
+        }
+        if(this.getStatus()){
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mensalidade paga com sucesso por " + this.getDono() + " no valor de " + v);
+        }else{
+            System.out.println("Impossível pagar uma conta fechada ! ");
+        }
     }
 
     //Metodos especiais↓
-    
     //CONSTRUTOR
     public ContaBanco() {
         this.setSaldo(0);
@@ -52,48 +104,50 @@ public class ContaBanco {
     public int getNumConta() {
         return this.numConta;
     }
-
+    
     public String getTipo() {
         return tipo;
     }
-
+    
     public String getDono() {
         return dono;
     }
-
+    
     public float getSaldo() {
         return saldo;
     }
 
-    public boolean isStatus() {
+    /**
+     * Todo metodo que trabalha com booleano em java retorna (is) então estava
+     * (isStatus) alterei para get por conta do aprendizado.
+     *
+     * @return
+     */
+    public boolean getStatus() {
         return status;
     }
-    
-    //SETTERS
 
+    //SETTERS
     public void setNumConta(int numConta) {
         this.numConta = numConta;
     }
-
+    
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-
+    
     public void setDono(String dono) {
         this.dono = dono;
     }
-
+    
     public void setSaldo(float saldo) {
         this.saldo = saldo;
     }
-
+    
     public void setStatus(boolean status) {
         this.status = status;
     }
     
-    public void status(){
-        System.out.println(this.saldo);
-        System.out.println(this.status);
-    }
+   
     
 }
--- JavaPooAula05/src/javapooaula05/JavaPooAula05.java
+++ JavaPooAula05/src/javapooaula05/JavaPooAula05.java
@@ -15,9 +15,29 @@ public class JavaPooAula05 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ContaBanco cb = new ContaBanco();
-        cb.status();
+        ContaBanco p1 = new ContaBanco();
+       
+        p1.setNumConta(1111);
+        p1.setDono("Jubileu");
+        p1.setSaldo(300);
+        p1.setTipo("CC");
+        p1.setStatus(true);
+        p1.pagarMensal();
         
+        p1.estadoAtual();
+        
+        System.out.println("------------------------------");
+        
+        ContaBanco p2 = new ContaBanco();
+        
+        p2.setNumConta(2222);
+        p2.setDono("Jubileia");
+        p2.setSaldo(500);
+        p2.setTipo("CP");
+        p2.setStatus(true);
+        p2.pagarMensal();
+        
+        p2.estadoAtual();
     }
     
 }
